6493116bf411c5f8deb88cc040423281a9b38109
aryanbhimani <aryan.bhimani.93@gmail.com>
2024-12-13 17:53:30 +0530

    notes

--- lib/Home /notes.dart
+++ lib/Home /notes.dart
@@ -1,1 +1,181 @@
 //aryan bhimani
+
+
+
+import 'package:flutter/material.dart';
+import 'package:google_gemini/google_gemini.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+const apiKey = "AIzaSyDBlZs66H4pr9cMT2NycHKgzPZ7MulT0Ao";
+
+class textWithImage extends StatefulWidget {
+  const textWithImage({
+    super.key,
+  });
+
+  @override
+  State<textWithImage> createState() => _textWithImageState();
+}
+
+class _textWithImageState extends State<textWithImage> {
+  bool loading = false;
+  List textAndImageChat = [];
+  List textWithImageChat = [];
+  File? imageFile;
+
+  final ImagePicker picker = ImagePicker();
+
+  final TextEditingController _textController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+
+  // Create Gemini Instance
+  final gemini = GoogleGemini(
+    apiKey: apiKey,
+  );
+
+  // Text only input
+  void fromTextAndImage({required String query, required File image}) {
+    setState(() {
+      loading = true;
+      textAndImageChat.add({
+        "role": "User",
+        "text": query,
+        "image": image,
+      });
+      _textController.clear();
+      imageFile = null;
+    });
+    scrollToTheEnd();
+
+    gemini.generateFromTextAndImages(query: query, image: image).then((value) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Mind Sphere", "text": value.text, "image": ""});
+      });
+      scrollToTheEnd();
+    }).onError((error, stackTrace) {
+      setState(() {
+        loading = false;
+        textAndImageChat
+            .add({"role": "Mind Sphere", "text": error.toString(), "image": ""});
+      });
+      scrollToTheEnd();
+    });
+  }
+
+  void scrollToTheEnd() {
+    _controller.jumpTo(_controller.position.maxScrollExtent);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        centerTitle: true,
+        title: const Text(
+          'AI Image Chat',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: textAndImageChat.length,
+              padding: const EdgeInsets.only(bottom: 20),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  isThreeLine: true,
+                  leading: CircleAvatar(
+                    child:
+                    Text(textAndImageChat[index]["role"].substring(0, 1)),
+                  ),
+                  title: Text(textAndImageChat[index]["role"]),
+                  subtitle: Text(textAndImageChat[index]["text"]),
+                  trailing: textAndImageChat[index]["image"] == ""
+                      ? null
+                      : Image.file(
+                    textAndImageChat[index]["image"],
+                    width: 90,
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            margin: const EdgeInsets.all(10),
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40.0),
+              border: Border.all(color: const Color.fromARGB(255, 0, 0, 0)),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _textController,
+                    decoration: InputDecoration(
+                      hintText: "Write a message",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide.none),
+                      fillColor: Colors.transparent,
+                    ),
+                    maxLines: null,
+                    keyboardType: TextInputType.multiline,
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.add_a_photo,color: Color.fromARGB(255, 0, 0, 0)),
+                  onPressed: () async {
+                    final XFile? image = await picker.pickImage(source: ImageSource.camera);
+                    setState(() {
+                      imageFile = image != null ? File(image.path) : null;
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.image,color: Color.fromARGB(255, 0, 0, 0)),
+                  onPressed: () async {
+                    final XFile? image =
+                    await picker.pickImage(source: ImageSource.gallery);
+                    setState(() {
+                      imageFile = image != null ? File(image.path) : null;
+                    });
+                  },
+                ),
+                IconButton(
+                  icon: loading
+                      ? const CircularProgressIndicator()
+                      : const Icon(Icons.send,color: Color.fromARGB(255, 0, 0, 0)),
+                  onPressed: () {
+                    if (imageFile == null) {
+                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                          content: Text("Please select an image")));
+                      return;
+                    }
+                    fromTextAndImage(
+                        query: _textController.text, image: imageFile!);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: imageFile != null
+          ? Container(
+        margin: const EdgeInsets.only(bottom: 80),
+        height: 150,
+        child: Image.file(imageFile ?? File("")),
+      )
+          : null,
+    );
+  }
+}
