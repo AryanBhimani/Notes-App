6d2b3fd453569df17a8ca14ee1c4a030f06b7031
aryanbhimani <aryan.bhimani.93@gmail.com>
2024-12-17 18:41:15 +0530

    new file SplashScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/Onboarding/SplashScreen.dart';
+import 'package:notes_app/SplashScreen/SplashScreen.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
